fc39baa74f7b9d73f3414ca9ca43ac0c394108b0
Brian Zauner <binrz00@hotmail.com>
2019-12-19 23:42:44 -0600

    should of taken the easy route with mysql

--- employee-tracker.js
+++ employee-tracker.js
@@ -1,7 +1,8 @@
+const employee = require("./model/models");
 import("sequelize");
+import("./model");
 const inquirer = require("inquirer");
 const cTable = require('console.table');
-const chooseEmployee = [{message: "which employee would you like to select",name: "employee"}];
 function toDoQuestions(){
 inquirer.prompt([{type:"list",message:"what would you like to do?",name: "toDo",choices:["view all employees","view all departments","view all roles",
 "add an employee","add a role","add a department","update an employee's role"]}]).then(function(data){
@@ -31,9 +32,10 @@ break;
 })
 }
 function viewEmployees(){
-
-
- // toDoQuestions();
+employee.findAll().then(function(data){
+  console.log(data);
+})
+ toDoQuestions();
 };
 function viewDepartments(){
   toDoQuestions();
@@ -42,36 +44,29 @@ function viewRoles(){
   toDoQuestions();
 };
 function addEmployee(){
-  
+  inquirer.prompt([{message:"what is the employee's first name?",name:"Fname"},{message:"what is the employee's last name?",name:"Lname"},
+{message:"what is the employee's title?",name:"title"},{message:"who is the employee's manager?",name:"manager"},{message:"how much does this employee make?",name:"salary"}])
+.then(function(res){
+  console.log(res.Fname,res.Lname,res.manager,res.title.res.salary);
+})
   toDoQuestions();
 };
 function addRole(){
-  inquirer.prompt(chooseEmployee).then(function(){
+  inquirer.prompt([{message:"what is the title of the new role?",name:"title"},{message:"what department does this role work in?",name:"dep"}]).then(function(){
   
   })
   toDoQuestions();
 };
 function promotion(){
+  inquirer.prompt([{type:"list",message:"who is the employee?",name:"emp",choices:[]},{message:"what is their new title?",name:"title"}]).then(function(){
+  
+  })
   toDoQuestions();
 };
 function addDepartment(){
-  inquirer.prompt(chooseEmployee).then(function(){
+  inquirer.prompt([{message:"what is the name of the new department?",name:"name"}]).then(function(){
   
   })
   toDoQuestions();
 };
 
-// bar_id: {
-//   type: Sequelize.INTEGER,
-
-//   references: {
-//     // This is a reference to another model
-//     model: Bar,
-
-//     // This is the column name of the referenced model
-//     key: 'id',
-
-//     // This declares when to check the foreign key constraint. PostgreSQL only.
-//     deferrable: Sequelize.Deferrable.INITIALLY_IMMEDIATE
-//   }
-// },
